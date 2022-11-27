32e4a5efc5e3ecacf2d30bddac4baf6c39a11295
PattynRuben <ruben.pattyn@student.howest.be>
2022-11-27 14:45:46 +0100

    flippen

--- script/app.js
+++ script/app.js
@@ -1,7 +1,17 @@
+'use strict';
 
-
+const listenToButton = function () {
+    document.querySelectorAll('.js-button').forEach((button) => {
+      button.addEventListener('click', function (e) {
+        console.log('button clicked');
+        document.querySelector('.c-flip--inner').classList.toggle('is-flipped');
+      });
+    });
+  };
+  
 const init = function(){
     console.log("App initialized");
+    listenToButton();
 }
 
 document.addEventListener("DOMContentLoaded", init);
